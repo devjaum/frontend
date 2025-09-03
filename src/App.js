9040e3d68fa9499c0b6677077478fc43e532d9d6
João Carlos <devjaum@gmail.com>
2025-09-02 21:01:33 -0300

    doodlePattern

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function ChatApp() {
   const [online, setOnline] = useState([]);
   const [showOnline, setShowOnline] = useState(false);
 
-  const BASE_URL = "http://localhost:3001";
+  const BASE_URL = "https://backend-04cn.onrender.com";
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
@@ -143,53 +143,58 @@ function ChatApp() {
 
   if (!loggedIn) {
     return (
-        <div className="flex flex-col items-center h-[100vh] justify-center bg-[#264048] text-[#A7FFFF] font-bold">
-          <h1 className="text-3xl font-bold m-5">Login</h1>
-
-          <input
+        <div 
+          style={{backgroundImage: "url('/doodlePatternD.png')"}}
+          className="flex flex-col items-center h-[100vh] justify-center bg-[#264048] text-[#A7FFFF] font-bold bg-[size:55%]">
+          <div className="flex flex-col backdrop-blur-md w-[85%] sm:w-[25%] items-center">
+            <h1 className="text-3xl font-bold m-5">Login</h1>
+            <input
             placeholder="Usuário"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="bg-[#1A2B30] p-1 m-1 rounded-md"
-          />
-          <input
+            className="bg-[#1A2B30] p-1 m-1 rounded-md w-[65%]"
+            />
+            <input
             type="password"
             placeholder="Senha"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="bg-[#1A2B30] p-1 m-1 rounded-md"
-          />
+            className="bg-[#1A2B30] p-1 m-1 rounded-md w-[65%]"
+            />
 
-          <div className="flex gap-3">
+            <div className="flex gap-3">
             <button
-              onClick={handleLogin}
-              className="m-2 px-4 py-1 bg-[#1A2B30] hover:bg-[#375E6A] rounded-md"
+            onClick={handleLogin}
+            className="m-2 px-4 py-1 bg-[#1A2B30] hover:bg-[#375E6A] rounded-md mb-5"
             >
-              Entrar
+            Entrar
             </button>
             <button
-              onClick={handleRegister}
-              className="m-2 px-4 py-1 bg-[#1A2B30] hover:bg-[#375E6A] rounded-md"
+            onClick={handleRegister}
+            className="m-2 px-4 py-1 bg-[#1A2B30] hover:bg-[#375E6A] rounded-md mb-5"
             >
-              Registrar
-            </button>
+            Registrar
+</button>
+              </div>
           </div>
         </div>
     );
   }
 
   return (
-    <div className="flex flex-col items-center h-auto min-h-[100vh] justify-center bg-[#264048] font-bold text-[#A7FFFF]">
-      <div className="flex flex-col w-[100%] items-end mr-5">
+    <div 
+      style={{backgroundImage: "url('/doodlePatternD.png')"}}
+      className="flex flex-col items-center h-auto min-h-[100vh] justify-center bg-[#264048] font-bold text-[#A7FFFF] bg-[size:50%]">
+      <div className="flex flex-row w-[100%] justify-center items-center mr-5">
         <button
           onClick={() => setShowOnline((prev) => !prev)}
-          className="px-4 py-2 bg-[#1A2B30] hover:bg-[#375E6A] rounded-md"
+          className="px-4 py-2 backdrop-blur-lg hover:bg-[#375E6A] rounded-md max-h-[40px]"
         >
           {showOnline ? "Esconder online" : "Mostrar online"}
         </button>
 
         {showOnline && (
-          <div className="border w-[200px] bg-[#1E293B] rounded-xl p-2 mb-4">
+          <div className="border w-[200px] backdrop-blur-sm rounded-xl p-2 text-center">
             <h2 className="text-white font-bold mb-2">Online:</h2>
             <ul className="text-gray-200">
               {online.map((nome, index) => (
@@ -202,25 +207,28 @@ function ChatApp() {
         )}
       </div>
 
-      <h1 className="text-3xl font-bold m-5">Chat Online</h1>
+      <h1 className="text-3xl font-bold m-3 backdrop-blur-lg">Chat Online</h1>
 
-      <div className="p-4 flex flex-col bg-[#1A2B30] border-[#375E6A] border w-[80%] sm:w-[50%] min-h-[50vh]">
+      <div 
+        className="p-4 flex flex-col bg-[#1A2B30]/50 backdrop-blur-sm border-[#375E6A] border w-[80%] sm:w-[80%] max-h-[50vh] overflow-y-auto ">
         {messages.map((msg, idx) => (
           <div className="flex justify-center" key={idx}>
             {msg.user === "Sistema" ? (
               <div className="flex flex-col items-center w-[100%]">
-                <p className="text-[#ff5151] mr-2">{msg.user}:</p>
-                <p className="break-words">{sistemChat(msg.content)}</p>
+                <div className="backdrop-blur-lg px-2 flex mb-5">
+                  <p className="text-[#ff5151] mr-2">{msg.user}:</p>
+                  <p className="break-words">{sistemChat(msg.content)}</p>
+                </div>
               </div>
             ) : msg.user === username ? (
               <div className="flex flex-row-reverse w-[100%]">
-                <p className="text-[#21FF30] max-w-[75%] break-words bg-[#375E6A] min-w-[35%] text-end mb-2 rounded-2xl px-3 py-1">
+                <p className="text-[#21FF30] max-w-[75%] break-words bg-[#375E6A] min-w-[35%] text-end mb-2 rounded-l-3xl px-3 pb-3 pt-1 rounded-tr-3xl ">
                   {msg.content}
                 </p>
               </div>
             ) : (
               <div className="flex flex-row w-[100%]">
-                <div className="flex flex-row bg-[#264048] max-w-[75%] min-w-[35%] mb-2  rounded-2xl px-3 py-1">
+                <div className="flex flex-row bg-[#264048] max-w-[75%] min-w-[35%] mb-2 rounded-r-3xl px-3 pb-3 pt-1 rounded-tl-3xl py-1">
                   <p className="text-[#FFB7B4] mr-2">{msg.user}:</p>
                   <p className="max-w-[75%] break-words ">{msg.content}</p>
                 </div>
@@ -230,17 +238,17 @@ function ChatApp() {
         ))}
       </div>
 
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center">
         <input
           placeholder="Mensagem"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           onKeyDown={(e) => e.key === "Enter" && sendMessage()}
-          className="bg-[#1A2B30] rounded-md text-[#A7FFFF] mt-5 mb-5 mr-0 pl-2 h-5"
+          className="bg-[#1A2B30] rounded-md text-[#A7FFFF] mx-3 py-1 px-3 my-2"
         />
         <button
           onClick={sendMessage}
-          className="h-6 w-6 mt-5 ml-2 bg-[#1A2B30] hover:bg-[#375E6A] rounded-full text-[#A7FFFF]"
+          className="mx-3 py-1 px-3 my-2 bg-[#1A2B30] hover:bg-[#375E6A] rounded-full text-[#A7FFFF]"
         >
           {">"}
         </button>
